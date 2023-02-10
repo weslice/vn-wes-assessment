0a2d1b485b12c7d5ce006e277851b4ef7b7d6c77
Wes <weslice@hotmail.com>
2023-02-10 14:31:44 +0100

    Fix main app sysout explanation

--- exercise-2/src/main/java/com/visualnuts/Execise2App.java
+++ exercise-2/src/main/java/com/visualnuts/Execise2App.java
@@ -16,14 +16,14 @@ public class Execise2App
 
         var countryService = new CountryService(objectMapper.readValue(inputStream, new TypeReference<>(){}));
 
-        System.out.printf("Languages in countries: %s %n", countryService.countCountries());
+        System.out.printf("Number of countries in the world: %s %n", countryService.countCountries());
         System.out.printf(
                 "Country with most official language spoken, but speaks German: %s %n",
                 countryService.findCountryMostOfficialLanguagesFilteredByLanguage(GERMAN).get().country()
         );
         System.out.printf("Amount of languages spoken: %s%n", countryService.countDistinctLanguageInCountries());
         System.out.printf(
-                "Amount of languages spoken: %s%n",
+                "Find the country whit Most official languages spoken: %s%n",
                 countryService.findCountryWithMostOfficialLanguages().get().country()
         );
         System.out.printf(
